3f4398a813e8cd626108fa31688140527b54a89f
v-yelina <heytridvaodin@gmail.com>
2022-12-08 10:49:28 +0100

    feat: add exit message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+import readline from "readline";
+
 const start = () => {
   const args = process.argv.slice(2);
   const username = args[0].startsWith("--username") ? args[0].split("=")[1] : undefined;
+
   console.log(`Welcome to the File Manager, ${username}!`);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.prompt();
+
+  rl.on("line", (line) => {
+    switch (line.trim()) {
+      case ".exit":
+        rl.close();
+        break;
+      default:
+        // sentence = line + "\n";
+        // writable.write(sentence);
+        // rl.prompt();
+        break;
+    }
+  }).on("close", () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  });
 };
 
 start();
