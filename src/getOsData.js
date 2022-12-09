5189f2e78c901e19957f0692b8696da0c11e2794
v-yelina <heytridvaodin@gmail.com>
2022-12-09 09:28:32 +0100

    feat: add info messages

--- src/getOsData.js
+++ src/getOsData.js
@@ -3,18 +3,23 @@ import os from "os";
 export const getOsData = (arg) => {
   switch (arg) {
     case "EOL":
+      console.log("Your default system End-Of-Line:");
       console.log(JSON.stringify(os.EOL));
       break;
     case "cpus":
+      console.log("Your host machine CPUs info:");
       console.log(os.cpus());
       break;
     case "homedir":
+      console.log("Your home directory:");
       console.log(os.homedir());
       break;
     case "username":
+      console.log("Your current system user name:");
       console.log(os.userInfo().username);
       break;
     case "architecture":
+      console.log("Your CPU architecture for which Node.js binary has compiled:");
       console.log(os.arch());
       break;
     default:
