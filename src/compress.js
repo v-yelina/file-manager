633ee5f54913c1b23871f68c5d3426a1db560471
unknown <heytridvaodin@gmail.com>
2022-12-11 09:56:01 +0100

    feat: add compress function

---
+++ src/compress.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import zlib from "zlib";
+import { errorHandler } from "./utils/errorHandler.js";
+
+export const compress = (fileToCompressUrl, destinationFileUrl) => {
+  const readable = fs.createReadStream(fileToCompressUrl);
+  const writable = fs.createWriteStream(destinationFileUrl);
+
+  const brotli = zlib.createBrotliCompress();
+
+  readable
+    .pipe(brotli)
+    .pipe(writable)
+    .on("finish", () => {
+      console.log("Successfully done compessing");
+      writable.end();
+    });
+
+  readable.on("error", (err) => {
+    errorHandler(err);
+  });
+  writable.on("error", (err) => {
+    errorHandler(err);
+  });
+  brotli.on("error", (err) => {
+    errorHandler(err);
+  });
+};
